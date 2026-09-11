e211a83f3562296c892883b92feb76df28ccbcb1
João Vitor de souza Ricci <JVRicci>
2026-09-11 16:31:07 -0300

    fix: change dto file name

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import "@/global.css";
 
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import { Provider as PaperProvider } from 'react-native-paper';
 import { pt, registerTranslation } from 'react-native-paper-dates';
 
@@ -9,7 +10,11 @@ registerTranslation('pt', pt)
 export default function Layout() {
   return (
       <PaperProvider>
-        <Stack screenOptions={{ headerShown: false }} />
+        <StatusBar 
+          backgroundColor="#f4511e"
+          barStyle="dark-content"
+        />
+        <Stack screenOptions={{ headerShown: false,  }} />
       </PaperProvider>
   )
 }
